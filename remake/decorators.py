7a2821ffd7c9705425e3be60050ccd01d292b209
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-03-11 11:20:32 +0000

    More sensible inner wrapper name

--- remake/decorators.py
+++ remake/decorators.py
@@ -4,14 +4,14 @@ import functools
 def remake_required(*, on_func_change=True, depends_on=tuple()):
     def wrapped(func):
         @functools.wraps(func)
-        def wrapped_f(*args, **kwargs):
+        def wrapped_inner(*args, **kwargs):
             return func(*args, **kwargs)
 
-        wrapped_f.is_remake_wrapped = True
-        wrapped_f.remake_func = func
-        wrapped_f.remake_on_func_change = on_func_change
-        wrapped_f.depends_on = depends_on
+        wrapped_inner.is_remake_wrapped = True
+        wrapped_inner.remake_func = func
+        wrapped_inner.remake_on_func_change = on_func_change
+        wrapped_inner.depends_on = depends_on
 
-        return wrapped_f
+        return wrapped_inner
 
     return wrapped
