eae4e30f788e6b9f1e534c01239ebcf921b0c02a
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-12-04 20:09:08 +0000

    Move some files about.

--- examples/ex4.py
+++ examples/ex4.py
@@ -11,18 +11,17 @@ def join_lines(path, prepend_text):
 
 class DependsOn1(TaskRule):
     inputs = {'in': 'data/inputs/in1.txt'}
-    outputs = {'out': 'data/outputs/ex_task_rule4/out1.txt'}
+    outputs = {'out1': 'data/outputs/ex_task_rule4/out1.txt'}
     depends_on = [join_lines]
 
     def rule_run(self):
-        self.outputs['out'].write_text(join_lines(self.inputs['in'], 'DependsOn1'))
+        self.outputs['out1'].write_text(join_lines(self.inputs['in'], 'DependsOn1'))
 
 
 class DependsOn2(TaskRule):
     inputs = DependsOn1.outputs
-    outputs = {'out': 'data/outputs/ex_task_rule4/out2.txt'}
+    outputs = {'out2': 'data/outputs/ex_task_rule4/out2.txt'}
     depends_on = [join_lines]
 
     def rule_run(self):
-
-        self.outputs['out'].write_text(join_lines(self.inputs['out'], 'DependsOn2'))
+        self.outputs['out2'].write_text(join_lines(self.inputs['out1'], 'DependsOn2'))
