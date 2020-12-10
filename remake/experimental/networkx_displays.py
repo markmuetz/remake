401213905bd99e800b5d7a395065f3c6cbedb3f1
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-12-10 22:00:42 +0000

    Get networkx_displays working again.

--- remake/experimental/networkx_displays.py
+++ remake/experimental/networkx_displays.py
@@ -1,16 +1,6 @@
 import networkx as nx
 import numpy as np
-
-
-def tasks_as_networkx_graph(task_ctrl):
-    assert task_ctrl.finalized
-    G = nx.DiGraph()
-    for task in task_ctrl.tasks:
-        G.add_node(task)
-
-        for prev_task in task_ctrl.prev_tasks[task]:
-            G.add_edge(prev_task, task)
-    return G
+import matplotlib.pyplot as plt
 
 
 def files_as_networkx_graph(task_ctrl):
@@ -24,17 +14,18 @@ def files_as_networkx_graph(task_ctrl):
 
 
 def display_task_status(task_ctrl):
-    import matplotlib.pyplot as plt
-    TG = tasks_as_networkx_graph(task_ctrl)
     pos = {}
+    for i, task in enumerate(task_ctrl.rescan_tasks):
+        pos[task] = np.array([-1, i])
     for level, tasks in task_ctrl.tasks_at_level.items():
         for i, task in enumerate(tasks):
             pos[task] = np.array([level, i])
 
     plt.clf()
-    nx.draw_networkx_nodes(TG, pos, task_ctrl.completed_tasks, node_color='k')
-    nx.draw_networkx_nodes(TG, pos, task_ctrl.running_tasks, node_color='g')
-    nx.draw_networkx_nodes(TG, pos, task_ctrl.pending_tasks, node_color='y')
-    nx.draw_networkx_nodes(TG, pos, task_ctrl.remaining_tasks, node_color='r')
-    nx.draw_networkx_edges(TG, pos)
+    nx.draw_networkx_nodes(task_ctrl.task_dag, pos, task_ctrl.rescan_tasks, node_color='c')
+    nx.draw_networkx_nodes(task_ctrl.task_dag, pos, task_ctrl.completed_tasks, node_color='k')
+    nx.draw_networkx_nodes(task_ctrl.task_dag, pos, task_ctrl.running_tasks, node_color='g')
+    nx.draw_networkx_nodes(task_ctrl.task_dag, pos, task_ctrl.pending_tasks, node_color='y')
+    nx.draw_networkx_nodes(task_ctrl.task_dag, pos, task_ctrl.remaining_tasks, node_color='r')
+    nx.draw_networkx_edges(task_ctrl.task_dag, pos)
     plt.pause(0.01)
