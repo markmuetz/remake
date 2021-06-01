0c56ed4790ce9f08153ec72ba726fb52fcd3d195
Mark Muetzelfeldt <markmuetz@gmail.com>
2021-06-01 16:17:25 +0100

    Add remake.__version__.

--- remake/__init__.py
+++ remake/__init__.py
@@ -6,3 +6,17 @@ from remake.task_rule import TaskRule
 from remake.task_query_set import TaskQuerySet
 from remake.remake_base import Remake
 from remake.special_paths import SpecialPaths
+from remake.version import VERSION
+
+__version__ = VERSION
+__all__ = [
+    'Task',
+    'TaskControl',
+    'RemakeOn',
+    'fmtp',
+    'TaskRule',
+    'TaskQuerySet',
+    'Remake',
+    'SpecialPaths'
+]
+
